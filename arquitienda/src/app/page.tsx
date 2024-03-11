3b3dd77962279c6e35ba38b797bd8b5076cc4a2f
gabriela luigi <luigigabriela68@gmail.com>
2024-03-11 17:18:48 -0500

    h

--- arquitienda/src/app/page.tsx
+++ arquitienda/src/app/page.tsx
@@ -6,21 +6,6 @@ export default function Home() {
   return (
     <main>
       <h1>hola</h1>
-      <nav>
-            <ul>
-
-              <Link href="/Analisis">
-                <li>Analisis</li>
-              </Link>
-              <Link href="./Inventario">
-                <li>Inventario</li>
-              </Link>
-              <Link href="./Nomina">
-                <li>Nomina</li>
-              </Link>
-            </ul>
-          </nav>
-  
     </main>
   );
 }
