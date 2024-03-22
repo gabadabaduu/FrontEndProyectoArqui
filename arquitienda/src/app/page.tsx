6226f385e2be18767fd82288cdb8d360664f4426
gabriela luigi <luigigabriela68@gmail.com>
2024-03-22 11:27:49 -0500

    caijitas :)))))))))))))))))

--- arquitienda/src/app/page.tsx
+++ arquitienda/src/app/page.tsx
@@ -1,12 +1,43 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Link from "next/link";
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './page.module.css';
 
-export default function Home() {
+interface BoxProps {
+  title: string;
+  content: string;
+}
+
+const Boxa = ({ title, content }: BoxProps) => {
+
+  return (
+    <div className={styles.boxa}>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+};
+const Box = ({ title, content }: BoxProps) => {
+
+  return (
+    <div className={styles.box}>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+const Home = () => {
   return (
     <main>
       <h1>hola</h1>
-      
+      <div className={styles.container}>
+        <Box title="Analysis" content="Content of Analysis page goes here." />
+        <Box title="Inventory" content="Content of Inventory page goes here." />
+        <Box title="Nomina" content="Content of Nomina page goes here." />
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
