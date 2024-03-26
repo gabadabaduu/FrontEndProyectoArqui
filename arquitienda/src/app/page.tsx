251adbbc84d0467ed81db899736b310091116e16
sebastian0912 <sebstiangc555@gmail.com>
2024-03-25 19:36:37 -0500

    Cambio Guarni desde el main para errores

--- arquitienda/src/app/page.tsx
+++ arquitienda/src/app/page.tsx
@@ -1,24 +1,25 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./page.module.css";
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './page.module.css';
+
 
 const Home = () => {
   return (
     <main>
-      <h1>hola</h1>
-      <header>
-        <nav>
-          <ul className="link-container">
-            <li>
-              <Link href="register">Registrarse</Link>
-            </li>
-            <li>
-              <Link href="login">Iniciar Sesión</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+   <h1>hola</h1>
+   <header>
+          <nav>
+            <ul className="link-container">
+              <li>
+                <Link href="register">Registrarse</Link>
+              </li>
+              <li>
+                <Link href="login">Iniciar Sesión</Link>
+              </li> 
+            </ul>
+          </nav>
+        </header>
     </main>
   );
 };
