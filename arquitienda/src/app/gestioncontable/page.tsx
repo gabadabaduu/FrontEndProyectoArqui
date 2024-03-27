ef85d0263bedc573bc82c80ccd8f91833dd88196
gabriela luigi <luigigabriela68@gmail.com>
2024-03-27 14:09:08 -0500

    ---

--- arquitienda/src/app/gestioncontable/page.tsx
+++ arquitienda/src/app/gestioncontable/page.tsx
@@ -1,6 +1,11 @@
+import Navbar from "../components/navbar";
+import Navmenu from "../components/navmenu";
+
 export default function Gestion() {
     return (
       <main>
+        <Navbar></Navbar>
+      <Navmenu></Navmenu>
         <h1>Gestion Contable</h1>
       </main>
     );
