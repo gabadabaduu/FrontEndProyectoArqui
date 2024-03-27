c77af1fd725d6e7286cd4b9608a1d410bbee8cf0
Daniel Millan <78319864+millandaniel@users.noreply.github.com>
2024-03-26 22:06:34 -0500

    Update PayPal As Portal Facturacion
    Se agregaron los botones que faltaban

--- arquitienda/src/app/paypal/page.tsx
+++ arquitienda/src/app/paypal/page.tsx
@@ -1,9 +1,73 @@
+import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+
+
 export default function paypal() {
   return (
-    <div className="centered-content">
-      <main>
-        <h1>Facturación</h1>
-      </main>
-    </div>
+    <main>
+      <br></br>
+      <br></br>
+      <section>
+        <center>
+          <h1 className='letraTitulosBlanco'>Portal</h1>
+          <h1 className='letraTitulosVerde'>Facturacion</h1>
+        </center>
+      </section>
+      <section>
+        <div className="contenedor">
+          <center>
+            <br></br>
+            <p className='letraNormal'>
+              Ingrese su información de pago para continuar con la compra de monto $$$$
+              <br></br><br></br>
+            </p>
+          </center>
+          <br></br>
+          <br></br>
+          <section>
+            <p className='pMargin'>
+              Nombre en la Tarjeta
+            </p>
+            <br></br>
+            <input type="text" className="textbox" placeholder="Ingrese el nombre que aparece en su tarjeta"></input>
+          </section>
+          <br></br>
+          <br></br>
+          <br></br>
+          <section>
+            <p className='pMargin'>
+              Numero de Tarjeta
+            </p>
+            <br></br>
+            <input type="text" className="textbox" placeholder="Ingrese el numero de 16 digitos de su tarjeta"></input>
+          </section>
+          <br></br>
+          <br></br>
+          <br></br>
+          <section>
+            <p className='pMargin'>
+              Numero de Tarjeta /// Expiration day
+            </p>
+            <br></br>
+            <input type="text" className="textboxMid" placeholder="MM/AA"></input>
+            <input type="text" className="textboxMid" placeholder="CVV"></input>
+          </section>
+          <br></br>
+          <br></br>
+          <center>
+            <button className="botonPago">Regresar</button>
+            <br></br>
+            <br></br>
+            <button className="botonPago">Pagar</button>
+          </center>
+        </div>
+      </section>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+    </main>
   );
 }
