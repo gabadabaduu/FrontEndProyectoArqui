418d5a85696196925ef36e54aed742c3082853f8
Daniel Millan <78319864+millandaniel@users.noreply.github.com>
2024-03-27 03:40:20 -0500

    Nav Usuario

---
+++ arquitienda/src/app/components/navbaruser.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./navbar.module.css";
+import Image from "next/image"; // Importa la etiqueta Image de Next.js
+
+const Navbar: React.FC = () => {
+    return (
+        <div>
+            <nav className={styles.navbar}>
+                <center>
+                    <Image src="/marcela.png" alt="Usuario" width={200} height={250} />
+                </center>
+                <ul className={styles.navbarList}>
+                    <li className={styles.navbarItem}>
+                        <Link href="/user" legacyBehavior>
+                            <a className={styles.navbarLink}>Usuario</a>
+                        </Link>
+                    </li>
+                    <li className={styles.navbarItem}>
+                        <Link href="/personal" legacyBehavior>
+                            <a className={styles.navbarLink}>Empleados</a>
+                        </Link>
+                    </li>
+                    <li className={styles.navbarItem}>
+                        <Link href="/Home" legacyBehavior>
+                            <a className={styles.navbarLink}>Página Principal</a>
+                        </Link>
+                    </li>
+                    <li className={styles.navbarItem}>
+                        <Link href="/" legacyBehavior>
+                            <a className={styles.navbarLink}>Cerrar Sesión</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
