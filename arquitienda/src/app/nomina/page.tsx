857edecee3b17f04af09eb3d5ca378f4b08d82ad
Daniel Millan <78319864+millandaniel@users.noreply.github.com>
2024-03-27 21:24:14 -0500

    Direccion De Botones page.tsx Nomina a Paginas Correspondientes

--- arquitienda/src/app/nomina/page.tsx
+++ arquitienda/src/app/nomina/page.tsx
@@ -2,9 +2,9 @@
 
 import React /* @client */ from "react";
 import Image from "next/image";
-import Navbar from "../components/navbar";
+import Navbar from "../components/navbaruser";
 import styles from "./nomina.module.css";
-import Navmenu from "../components/navmenu";
+import Link from "next/link";
 
 interface Empleado {
   id: number;
@@ -19,37 +19,40 @@ const empleados: Empleado[] = [
     id: 1,
     name: "Mariana Restrepo",
     cargo: "Secretaria",
-    image: "/marianamov.gif",
+    image: "/marianamov.gif", // Aquí proporciona la ruta de la imagen ubicada en public
     sueldo: 1250000,
   },
   {
     id: 2,
     name: "Beatriz Pinzon",
     cargo: "Secretaria",
-    image: "/bettymov.gif",
+    image: "/bettymov.gif", // Aquí proporciona la ruta de la imagen ubicada en public
     sueldo: 1250000,
   },
   {
     id: 3,
     name: "Armando Mendoza",
     cargo: "CEO",
-    image: "/armandomov.gif",
+    image: "/armandomov.gif", // Aquí proporciona la ruta de la imagen ubicada en public
     sueldo: 9584000,
   },
   {
     id: 4,
     name: "Mario Calderon",
     cargo: "Sub-Gerente",
-    image: "/mariomov.gif",
+    image: "/mariomov.gif", // Aquí proporciona la ruta de la imagen ubicada en public
     sueldo: 7963458,
   },
 ];
 
+
 export default function Nomina() {
   const [isInfoVisible, setIsInfoVisible] = React.useState(false);
 
-  const [selectedEmpleado, setselectedEmpleado] =
-    React.useState<Empleado | null>(null);
+
+  const [selectedEmpleado, setselectedEmpleado] = React.useState<Empleado | null>(
+    null
+  );
 
   const handleMouseEnter = () => {
     setIsInfoVisible(true);
@@ -59,6 +62,7 @@ export default function Nomina() {
     setIsInfoVisible(false);
   };
 
+  // Función para manejar el clic en un producto
   const handleProductClick = (empleado: any) => {
     setselectedEmpleado(empleado);
   };
@@ -67,10 +71,10 @@ export default function Nomina() {
     <div className={styles.pageLayout}>
       <Navbar />
       <main className={styles.Container}>
-        <h1 className={styles.titulo}>Nomina</h1>
-        <Navmenu />
         <div className={styles.contenido}>
-          <div className={styles.encabezado}></div>
+          <div className={styles.encabezado}>
+            <h1 className={styles.titulo}>Empleados</h1>
+          </div>
           <div className={styles.listadoYDetalle}>
             <div className={styles.listado}>
               {empleados.map((empleado) => (
@@ -90,12 +94,17 @@ export default function Nomina() {
                     <span>Sueldo: ${empleado.sueldo}</span>
                     <span>Cargo: {empleado.cargo}</span>
                   </div>
+
                 </div>
+
               ))}
+
             </div>
 
             {selectedEmpleado && (
-              <div className={styles.detalle}>
+              <div
+                className={styles.detalle}
+              >
                 <center>
                   <Image
                     src={selectedEmpleado.image}
@@ -103,12 +112,16 @@ export default function Nomina() {
                     width={400}
                     height={400}
                     layout="intrinsic"
-                  />
-                </center>
+                  /></center>
                 <center>
                   <br></br>
-                  <button className="botonPago">Pagar Nomina</button>
-                  <button className="botonPago">Cancelar</button>
+                  <Link href="/paypal" legacyBehavior>
+                    <button className="botonPago">Pagar Nomina</button>
+                  </Link>
+                  <tab>||</tab>
+                  <Link href="/paypal" legacyBehavior>
+                    <button className="botonPago">Cancelar</button>
+                  </Link>
                 </center>
                 <div
                   className={styles.info}
@@ -127,4 +140,5 @@ export default function Nomina() {
       </main>
     </div>
   );
+
 }
